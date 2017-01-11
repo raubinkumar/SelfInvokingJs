4eb77cdb10bc3c0b5b95a0610ebfda868cc6a340
raubinkumar <raubinkumar@gmail.com>
2017-01-11 08:21:49 +0530

    Create selfinvoke.js

---
+++ selfinvoke.js
@@ -0,0 +1,8 @@
+(function(){
+var para = document.createElement("h1");
+var node = document.createTextNode("Function is executed sucessfully..");
+para.appendChild(node);
+
+var element = document.getElementById("div1");
+element.appendChild(para)
+})();
